45971563c57d5fb2cdad6a590367915947cac1c4
prithvikings <prithvi07raj07@gmail.com>
2025-11-07 20:17:24 +0530

    Refactor Redis configuration to use environment variables

--- backend/config/env.js
+++ backend/config/env.js
@@ -7,8 +7,8 @@ export const ENV = {
   MONGO_URI: process.env.MONGO_URI || '',
   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret_key',
   FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
-  REDIS_PORT: process.env.REDIS_PORT || 14787,
-  REDIS_HOST: process.env.REDIS_HOST || 'redis-14787.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
-  REDIS_USERNAME: process.env.REDIS_USERNAME || 'default',
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD || '0P8xP0XbhIJBn9rrAWWpwyHRfX5LV2fm',
+  REDIS_PORT: process.env.REDIS_PORT ,
+  REDIS_HOST: process.env.REDIS_HOST ,
+  REDIS_USERNAME: process.env.REDIS_USERNAME ,
+  REDIS_PASSWORD: process.env.REDIS_PASSWORD ,
 };
